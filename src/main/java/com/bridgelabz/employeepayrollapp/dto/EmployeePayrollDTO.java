59e185fc141b6266da171e97db2b2e9299539e67
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-09-02 00:28:20 +0530

    Added:User Friendly Error in case of Employee Id not Found

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,13 +1,9 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
-
-import lombok.Data;
-
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
-@Data
 public class EmployeePayrollDTO {
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name is Invalid")
     @NotEmpty(message = "Employee name cannot be null")
